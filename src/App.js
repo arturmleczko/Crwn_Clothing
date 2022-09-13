8616b34681d0dc8a68213853a1977493369663c4
Artur Mleczko <arturmleczko199@gmail.com>
2022-09-13 21:31:40 +0200

    feat: Adds a part of the login support in saga

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
+
+import { checkUserSession } from './store/user/user.action';
+
 import { getCurrentUser } from './utils/firebase/firebase.utils';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getCurrentUser().then((user) => console.log(user));
+    dispatch(checkUserSession());
   }, [dispatch]);
 
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
